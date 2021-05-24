0fe349cb5ffd0487bede01c52343dedd3d19721d
abnerjs <abner.js05@gmail.com>
2021-05-24 18:55:24 -0300

    canvas added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Calculator from './main/Calculator';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <div className='title-text inline-main'>
+      Calculator
+    </div>
+    <Calculator />
+  </div>,
   document.getElementById('root')
 );
 
